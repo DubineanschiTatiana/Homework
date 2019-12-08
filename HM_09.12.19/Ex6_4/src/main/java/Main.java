f1e4b7eb631a4cc80d89bd0a4c14303ea1400d3c
DubineanschiTatiana <dubineanschi.tatiana@gmail.com>
2019-12-08 17:05:14 +0200

    update 09.12.19

---
+++ HM_09.12.19/Ex6_4/src/main/java/Main.java
@@ -0,0 +1,15 @@
+
+
+
+public class Main {
+ public static void main(String... args) {
+String [][] array= new String[9][9];
+for (int i=0;i<array.length;i++){
+    for (int j=0;j<array.length;j++){
+        System.out.print("- ");
+    }
+    System.out.println("- ");
+}
+
+    }   
+}
