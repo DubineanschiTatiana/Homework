8ddb2ede9ed7af4c975cdaadc1b2d0fb35c55ac3
DubineanschiTatiana <dubineanschi.tatiana@gmail.com>
2019-12-18 12:14:16 +0200

    add ex 9.2

---
+++ classwork/convertarea/src/main/java/Dog.java
@@ -0,0 +1,13 @@
+
+public class Dog {
+private static int counter;
+
+static void count() {
+Dog.counter++;
+}
+static void printcounter(){
+    System.out.println("Times:"+Dog.counter);}
+
+
+
+}
--- mavenproject1/src/main/java/Computer.java
+++
@@ -1,9 +0,0 @@
-import edu.tekwill.lesson.computer.components.*;
-package edu.tekwill.lesson.computer;
-
-public class Computer {
-    Processor pr;
-    Motherboard mo;
-    HardDrive hd;
-   
-}
